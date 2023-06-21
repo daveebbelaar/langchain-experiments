052b18e0c021c734d1d37ed7b292e4b66128fffc
lxMBxl <132673030+lxMBxl@users.noreply.github.com>
2023-06-21 19:37:34 -0400

    Update app.py

--- slack/app.py
+++ slack/app.py
@@ -1,11 +1,23 @@
 import os
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+from slack_sdk.signature import SignatureVerifier
 from slack_bolt.adapter.flask import SlackRequestHandler
 from slack_bolt import App
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, request
+from flask import Flask, request, abort
 from functions import draft_email
+import logging
+from functools import wraps
+import time
+import sys
+
+# Configure the logging level and format
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    stream=sys.stdout,
+)
 
 # Load environment variables from .env file
 load_dotenv(find_dotenv())
@@ -17,11 +29,39 @@ SLACK_BOT_USER_ID = os.environ["SLACK_BOT_USER_ID"]
 
 # Initialize the Slack app
 app = App(token=SLACK_BOT_TOKEN)
+signature_verifier = SignatureVerifier(SLACK_SIGNING_SECRET)
 
 # Initialize the Flask app
-# Flask is a web application framework written in Python
 flask_app = Flask(__name__)
 handler = SlackRequestHandler(app)
+
+
+def require_slack_verification(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not verify_slack_request():
+            abort(403)
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+def verify_slack_request():
+    # Get the request headers
+    timestamp = request.headers.get("X-Slack-Request-Timestamp", "")
+    signature = request.headers.get("X-Slack-Signature", "")
+
+    # Check if the timestamp is within five minutes of the current time
+    current_timestamp = int(time.time())
+    if abs(current_timestamp - int(timestamp)) > 60 * 5:
+        return False
+
+    # Verify the request signature
+    return signature_verifier.is_valid(
+        body=request.get_data().decode("utf-8"),
+        timestamp=timestamp,
+        signature=signature,
+    )
 
 
 def get_bot_user_id():
@@ -68,14 +108,18 @@ def handle_mentions(body, say):
 
     mention = f"<@{SLACK_BOT_USER_ID}>"
     text = text.replace(mention, "").strip()
+    logging.info("Received text: " + text.replace("\n", " "))
 
-    say("Sure, I'll get right on that!")
+    say("Sure, I'll get right on that OG xMBx!")
     # response = my_function(text)
     response = draft_email(text)
+    logging.info("Generated response: " + response.replace("\n", " "))
     say(response)
 
 
+# Demo
 @flask_app.route("/slack/events", methods=["POST"])
+@require_slack_verification
 def slack_events():
     """
     Route for handling Slack events.
@@ -84,9 +128,11 @@ def slack_events():
     Returns:
         Response: The result of handling the request.
     """
+
     return handler.handle(request)
 
 
 # Run the Flask app
 if __name__ == "__main__":
-    flask_app.run()
+    logging.info("Flask app started")
+    flask_app.run(host="0.0.0.0", port=8005)
