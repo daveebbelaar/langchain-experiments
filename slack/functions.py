9a312e036d62b0cf0364008236ecd33387ffdcac
lxMBxl <132673030+lxMBxl@users.noreply.github.com>
2023-06-25 05:42:32 -0400

    Update functions.py

--- slack/functions.py
+++ slack/functions.py
@@ -1,32 +1,23 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
-from dotenv import find_dotenv, load_dotenv
 from langchain.prompts.chat import (
-     ChatPromptTemplate,
+    ChatPromptTemplate,
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
-from functions import draft_email 
 )
+from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-
-def draft_email(user_input, name="MB Banks"):
+def draft_email(user_input, name="Ben & Gpt4"):
     chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=1)
-    
 
     template = """
-    
     You are a helpful assistant that drafts an email reply based on an a new email.
-    
     Your goal is to help the user quickly create a perfect email reply.
-    
     Keep your reply short and to the point and mimic the style of the email so you reply in a similar manner to match the tone.
-    
     Start your reply by saying: "Hi {name}, here's a draft for your reply:". And then proceed with the reply on a new line.
-    
     Make sure to sign of with {signature}.
-    
     """
 
     signature = f"Kind regards, \n\{name}"
